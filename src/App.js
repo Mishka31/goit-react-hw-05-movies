2f4a2fd4d8e4690b1943b648084ad93bcc8c82b4
Misha Krasnonos <muzishutka7@gmail.com>
2021-09-19 01:54:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div className="App">Test</div>;
-}
+import React from "react";
+import { Route, NavLink, Switch } from "react-router-dom";
+import Home from "./pages/home.js";
+import Search from "./pages/Search.js";
+import Films from "./pages/films.js";
+// import NotFound from "./pages/NotFound.js";
+
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <NavLink exact to="/" className="NavLink" activeClassName="Link--active">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/films" className="NavLink" activeClassName="Link--active">
+          Films
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/search" className="NavLink" activeClassName="Link--active">
+          Search
+        </NavLink>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/search" component={Search} />
+      <Route path="/films" component={Films} />
+      <Route component={Home} />
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NonFound = () => {
+  return <h1>406 - Not Found!</h1>;
+};
+
+export default NonFound;
